606e3c010754c19aae7b6f7f6770cd416d4e683d
Marine Baron <marine17@marinealaplage.dev>
2018-08-14 14:21:30 +0200

    rm couocu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
-import { CoucouComponent } from './coucou/coucou.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostListItemComponent,
-    CoucouComponent
+    PostListItemComponent
   ],
   imports: [
     BrowserModule
